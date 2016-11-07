88ffd071d7caf1f092b4792e3a0c829e55a40d62
jpelgrims <jellepelgrims+github@hotmail.com>
2016-11-07 16:58:35 +0100

    Second attempt
    Statwebgen now uses markdown for content. The markdown is converted to
    html and inserted into the page template.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # statwebgen
-A simple static website generator.
+A simple static website generator, geared towards text-heavy content.
+
+## Usage
+The static website is generated in the specified output directory, using the source files found in the input directory. File hierarchy is maintained during conversion.
+
+A new webpage can be added by simply creating a new markdown file in the input directory and adding content. Run the website generator when ready. The markdown in the file will automatically be converted to html and stored in the output directory. Page layout and styling is defined by the page template.
+
+The default page template looks like this:
+
+~~~~html
+<!DOCTYPE html>
+<html>
+    <%head%>
+    <body>
+        <%header%>
+        <div class="page-content">
+            <%page%>
+        </div>
+        <%footer%>
+    </body>
+</html>
+~~~~
+
+The `<%...%>` tags are replaced with the contents of the file with the same name as the tag. The `<%header%>` tag, for example,  will be replaced by the contents in `header.html`. The `<%page%>` tag is replaced by the markdown file's content (converted to html).
--- statwebgen.py
+++ statwebgen.py
@@ -1,31 +1,23 @@
 import os, os.path
 import shutil
 import glob
+import markdown
+# 'Invisible' dependency on Pygments for markdown highlighting
 
 INPUT_DIR = r'c:\users\jelle\desktop\website'
 OUTPUT_DIR = r'c:\users\jelle\documents\github\jpelgrims'
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
-EXCLUDE_DIRS = [WORKING_DIR.lower(),
-                r'c:\users\jelle\desktop\website\.idea',
-                r'c:\users\jelle\desktop\website\.idea\copyright']
 
 files = []
 pattern   = "*"
 
-variables = {
-    '<%head%>': 'head.html',
-    '<%header%>': 'header.html',
-    '<%footer%>': 'footer.html'}
-
+# Get list of all files in input directory and subdirectorie
 for dir,_,_ in os.walk(INPUT_DIR):
-    # Avoid adding statwebgen files when statwebgen in project directory
-    if dir.lower() in EXCLUDE_DIRS:
-        continue
     files.extend(glob.glob(os.path.join(dir,pattern)))
 
-# Sort files into html and other
-html_files = [file for file in files if ".html" in file and os.path.isfile(file)]
-other_files = [file for file in files if not ".html" in file and os.path.isfile(file)]
+# Sort files into markdown and other
+md_files = [file for file in files if ".md" in file and os.path.isfile(file)]
+other_files = [file for file in files if not ".md" in file and not ".html" in file and os.path.isfile(file)]
 
 # Move other files to output directory
 for file in other_files:
@@ -36,22 +28,29 @@ for file in other_files:
         pass
     shutil.copyfile(file, copyfile)
 
-# Change variables with corresponding html and write to output directory
-for html_file in html_files:
-    with open(html_file, 'r') as file:
-        content = file.read()
-    for variable, filepath in variables.items():
-        with open((WORKING_DIR + '\\' + filepath), 'r') as file:
-            html = file.read()
-        content = content.replace(variable, html)
+# Load page template
+with open((INPUT_DIR + '\\' + 'template.html'), 'r') as file:
+    template = file.read()
 
-    outputfile = os.path.dirname(OUTPUT_DIR + html_file.lower().replace(INPUT_DIR,'')) + '\\' + os.path.basename(html_file)
+# Loop over all content files
+for md_file in md_files:
+    with open(md_file, 'r') as file:
+        content = file.read()
+
+    # Convert markdown to html
+    post = markdown.markdown(content, extensions=['codehilite', 'fenced_code', 'toc'])
+    
+    # Use page template
+    page = template.replace('<%post%>', post)
+
+    # Save to output directory
+    outputfile = os.path.dirname(OUTPUT_DIR + md_file.lower().replace(INPUT_DIR,'')) + '\\' + os.path.splitext(os.path.basename(md_file))[0] + '.html'
     try:
         os.makedirs(os.path.dirname(outputfile))
     except:
         pass
     with open(outputfile, 'w') as file:
-        file.write(content)
+        file.write(page)
 
 print('Succesfully created static site @', OUTPUT_DIR)
 
