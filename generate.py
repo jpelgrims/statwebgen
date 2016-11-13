77bf84f1746299982b2978fab0df8a2122bd96ac
jpelgrims <jellepelgrims+github@hotmail.com>
2016-11-13 16:13:29 +0100

    Javascript, output and settings improvements
    Added automated JS script includes in html head.
    More detailed output.
    Settings are now read from a config file.

--- generate.py
+++ generate.py
@@ -1,19 +1,27 @@
 import os, os.path
 import shutil
 import glob
+import json
 import markdown
 import datetime
 # 'Invisible' dependency on Pygments for markdown highlighting
 
 from statwebgen import Page
 
-
-INPUT_DIR = r'c:\users\jelle\desktop\website'
-OUTPUT_DIR = r'c:\users\jelle\documents\github\jpelgrims'
+print('StatWebGen 0.1 ready.')
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
-EXCLUDE_DIR = [r'c:\users\jelle\desktop\website\drafts']
 
-print("Converting Markdown files found in \'{}\' into static website at \'{}\'".format(INPUT_DIR, OUTPUT_DIR))
+print('Loading configuration file...', end=" ")
+with open(WORKING_DIR + '\config.json', 'r') as file:
+    settings = json.loads(file.read())
+
+PROJECT_NAME = settings['PROJECT_NAME']
+INPUT_DIR = settings['INPUT_DIR']
+OUTPUT_DIR = settings['OUTPUT_DIR']
+EXCLUDE_DIR = settings['EXCLUDE_DIR']
+
+print("done.")
+print("")
 
 files = []
 pattern   = "*"
@@ -22,11 +30,17 @@ pattern   = "*"
 for dir,_,_ in os.walk(INPUT_DIR):
     files.extend(glob.glob(os.path.join(dir,pattern)))
 
+print("Project: '{}', consisting of {} files".format(PROJECT_NAME, len(files)))
+print("Input directory: {}".format(INPUT_DIR))
+print("Output directory: {}".format(OUTPUT_DIR))
+print("")
+
 # Sort files into markdown and other
 md_files = [file for file in files if ".md" in file and os.path.isfile(file) and not "drafts" in file]
 other_files = [file for file in files if not ".md" in file and not ".html" in file and os.path.isfile(file) and not file in EXCLUDE_DIR]
 
 # Copy other files to output directory
+print('Copying {} non-markdown files to output directory...'.format(len(other_files)), end=" ")
 for file in other_files:
     copyfile = os.path.dirname(OUTPUT_DIR + file.lower().replace(INPUT_DIR,'')) + '\\' + os.path.basename(file)
     try:
@@ -34,14 +48,18 @@ for file in other_files:
     except:
         pass
     shutil.copyfile(file, copyfile)
+print("done.")
 
 # Turn all markdown files into Page objects
 template_file = INPUT_DIR + '\\' + 'template.html'
 pages = []
 
+print("Processing markdown pages...")
+
 for md_file in md_files:
     page = Page(template_file)
     page.load(filepath=md_file)
+    print("   * {}".format(os.path.basename(page.filepath)))
     pages.append(page)
     outputfile = os.path.dirname(OUTPUT_DIR + md_file.lower().replace(INPUT_DIR,'')) + '\\' + os.path.splitext(os.path.basename(md_file))[0] + '.html'
     page.save(outputfile)
@@ -62,6 +80,7 @@ for post in posts:
     else:
         teasers[post.created].append(post.teaser)
 
+print("Building blog page...", end=" ")
 blog = Page(template_file)
 blog.title = "Blog"
 blog.description = "List of all published posts"
@@ -75,8 +94,9 @@ for category, titles in categories.items():
 
 blog.content = "".join(blog.content)
 blog.save(OUTPUT_DIR + '\\' + 'blog.html')
+print("done.")
 
-
+print("Building index page...", end=" ")
 index = Page(template_file)
 index.title = "Index"
 index.description = "Homepage"
@@ -87,6 +107,7 @@ for created_date, teasers in sorted(teasers.items(), key=lambda t: t[0], reverse
 
 index.content = "".join(index.content)
 index.save(OUTPUT_DIR + '\\' + 'index.html')
-
-print('Succesfully created static site @', OUTPUT_DIR)
+print('done.')
+print('')
+print('Static site creation succesfull!')
 
--- statwebgen.py
+++ statwebgen.py
@@ -12,6 +12,7 @@ class Page:
         self.description = ""
         self.created = None
         self.updated = None
+        self.javascript = []
         self.content = []
         self.teaser = ""
 
@@ -23,7 +24,7 @@ class Page:
                 page = file.readlines()
         
         metadata = {}
-        for i in range(6):
+        for i in range(7):
             temp = page[i].split(":")
             tag, data = temp[0].lower(), temp[1].strip()
             metadata[tag] = data
@@ -34,23 +35,32 @@ class Page:
         self.category = metadata['category']
         self.created = datetime.datetime.strptime(metadata['created'], "%d/%m/%Y")
         self.updated = datetime.datetime.strptime(metadata['updated'], "%d/%m/%Y")
+        self.javascript = [item.strip() for item in metadata['javascript'].split(",")]
+
         if self.type == 'page':
-            self.content = "#" + self.title + "\n\n" + "".join(page[7:])
+            self.content = "#" + self.title + "\n\n" + "".join(page[8:])
         elif self.type =='post':
-            self.content = "#" + self.title + "\n\n**Table of contents**\n" + "\n[TOC]\n\n" +  "".join(page[7:])
+            self.content = "#" + self.title + "\n\n**Table of contents**\n" + "\n[TOC]\n\n" +  "".join(page[8:])
             post_link = "[Read more...](/posts/" + self.title.lower().replace(" ", "_") + ".html)"
             self.teaser = "\n".join(["## " + self.title, 
                                     "<small>Created on " + self.created.strftime("%d/%m/%Y") + ", last updated on " + self.updated.strftime("%d/%m/%Y") + "</small>\n", 
-                                    page[7], post_link, "\n"])
+                                    page[8], post_link, "\n"])
 
     def _to_html(self, template_file):
         with open(template_file, 'r') as file:
             template = file.read()
 
-        html = markdown.markdown(self.content, extensions=['codehilite', 'fenced_code', 'toc'])
+        html = markdown.markdown(self.content, extensions=['codehilite', 'fenced_code', 'toc', 'extra'])
+        script_includes = "".join(["<script type='text/javascript' async src='/javascript/{}'></script>\n".format(script) for script in self.javascript if not 'mathjax.js' in script])
+        
+        #Check for mathjax because it needs a special include due to it being loaded from an external source
+        for script in self.javascript:
+            if 'mathjax.js' in script:
+                script_includes += "<script type='text/javascript' async src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'></script>\n"
+                break
 
         html_page = template
-        for tag, tag_content in {'<%post%>': html, '<%title%>': self.title, '<%description%>': self.description}.items():
+        for tag, tag_content in {'<%scripts%>': script_includes, '<%post%>': html, '<%title%>': self.title, '<%description%>': self.description}.items():
             html_page = html_page.replace(tag, tag_content)
         
         return html_page
